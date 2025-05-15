49c14c5efd6af3c1f483553ac96bc5db326a18fb
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-05-15 18:54:00 +0530

    Header updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = ({menuOpen, setMenuOpen}) => {
             
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white">DEV<span className="text-blue-500">.tech</span></a>
+                    <a href="#home" className="font-mono text-xl font-bold text-white"><span className="text-blue-500">Portfolio</span></a>
                 
 
             {/* Icon for mobile devices */}
