5f84784838af4dd43066e45ac00cee5b2efa6293
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-05-16 01:34:11 +0530

    Fix: Style update

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
 
             <RevealOnScroll>
 
-                <div className="px-4 w-150">
+                <div className="px-4 w-full md:w-150">
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-600 bg-clip-text text-transparent text-center">Get In Touch</h2>
                     <form className="space-y-6" onSubmit={handleSubmit}>
                         <div className="relative">
