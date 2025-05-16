deea344659c0bdd90facd773e4552a064f886261
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-05-17 00:04:18 +0530

    Fix: About section fix

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
 
     const backendSkills = ["Node.js", "MongoDB", "Express.js", "SQL"];
 
-    const otherSkills = ["Python", "AWS", "Dockers", "Kubernetes", "VS Code" ,"Git", "ServiceNOW", "Jira"];
+    const otherSkills = ["Python", "AWS", "Dockers", "Kubernetes", "Git"];
+
+    const tools = ["VS Code" ,"Github", "Postman", "ServiceNOW", "Jira"];
 
 
     return (
@@ -44,7 +46,7 @@ const About = () => {
                         </div>
 
                         <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                            <h3 className="text-xl font-bold mb-4">Tools / Other Skills</h3>
+                            <h3 className="text-xl font-bold mb-4">Other Skills</h3>
                             <div className="flex flex-wrap gap-2">
                                 {otherSkills.map((tech, key) => (
                                     <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
@@ -53,6 +55,18 @@ const About = () => {
                                 ))}
                             </div>
                         </div>
+
+                        <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                            <h3 className="text-xl font-bold mb-4">Tools</h3>
+                            <div className="flex flex-wrap gap-2">
+                                {tools.map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59, 130, 2246, 0.2)] transition">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                        </div>
+
                     </div>
 
                 </div>
