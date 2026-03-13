19e3585750b17dbcb544a3e2d67d5ca0bdc844e2
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2026-03-13 21:52:06 +0530

    Updating the contacts page

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -49,7 +49,7 @@ const Contact = () => {
         }else{
             setLoading(true);
             emailjs.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, import.meta.env.VITE_PUBLIC_KEY).then((result) => {
-            toast.info(result);
+            toast.info(result, "Message Sent Successfully!");
             setFormData({name: "", email:"", message: ""});
             setErrors({});
         })
