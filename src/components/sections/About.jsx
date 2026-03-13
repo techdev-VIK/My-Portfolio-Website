4df1bbf8437642850d7b613cad94d14cb2d4f137
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2026-03-13 21:39:48 +0530

    styling changes

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
                 <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-600 bg-clip-text text-transparent text-center">About Me</h2>
 
 
-                <div className="p-6 mb-6 rounded-xl border border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                <div className="p-6 mb-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                     <p className="text-gray-400 text-lg mb-8 max-w-xl mx-auto mt-6">Passionate software developer with experience in building scalable web applications and creating innvoative solutions.</p>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -71,7 +71,7 @@ const About = () => {
 
                 </div>
  
-                            <div className="p-6 mb-6 rounded-xl border border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                            <div className="p-6 mb-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                                 <h3 className="text-xl font-bold mb-5"> 🏫 Education</h3>
                                 <ul className="list-disc list-inside text-gray-400 space-y-2">
                                     <li>
@@ -93,7 +93,7 @@ const About = () => {
                                 </ul>
                             </div>
 
-                            <div className="p-6 rounded-xl border border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
+                            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59, 130, 246, 0.1)] transition-all">
                                 <h3 className="text-xl font-bold mb-5"> 💼 Work Experience</h3>
 
                                 <ul className="list-disc list-inside text-gray-400 space-y-1">
